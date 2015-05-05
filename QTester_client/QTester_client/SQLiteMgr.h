8c3637ffb7966a0eb1a1671e48b4a7380653bd6a
Khaidayev Yroslav <khaidayev@gmail.com>
2015-05-05 19:56:01 +0600

    MODIFIES

--- QTester_client/QTester_client/SQLiteMgr.h
+++ QTester_client/QTester_client/SQLiteMgr.h
@@ -8,8 +8,8 @@ public:
 	~SQLiteMgr();
 
 	void init(const QString &dbHost,
-					 const QString &dbUser,
-					 const QString &dbPass);
+		      const QString &dbUser,
+			  const QString &dbPass);
 	
 
 	virtual bool createTable(QString &tableName, DataMap &data) override;
@@ -19,12 +19,12 @@ public:
 						qint64 limit = 25) override;
 
 	virtual QSqlQuery insert(QString& tableName_to,
-		QStringList& fields_to,
-		QString& tableName_from,
-		QStringList& fields_from,
-		QString& where_field,
-		QString& where_value,
-		qint64 limit) override;
+							 QStringList& fields_to,
+							 QString& tableName_from,
+							 QStringList& fields_from,
+							 QString& where_field,
+							 QString& where_value,
+							 qint64 limit) override;
 	//static bool createTable(QString &tableName, DataMap &data);
 
 	virtual bool sescionConfigurate(QStringList &parameters, QStringList &values, qint64 limit);
